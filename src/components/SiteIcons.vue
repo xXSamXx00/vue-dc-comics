18b9c573b8e695912f890adf6bc05d70056e0e2e
xXSamXx00 <samudichio@gmail.com>
2021-11-11 18:27:18 +0100

    Bonus Completato

---
+++ src/components/SiteIcons.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="menu_dc">
+        <div class="container">
+            <div class="icon">
+                <div class="img">
+                    <img src="../assets/buy-comics-digital-comics.png" alt>
+                </div>
+                <h3>Digital comics</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img src="../assets/buy-comics-merchandise.png" alt>
+                </div>
+                <h3>DC merchandise</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img src="../assets/buy-comics-subscriptions.png" alt>
+                </div>
+                <h3>Subscription</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img src="../assets/buy-comics-shop-locator.png" alt>
+                </div>
+                <h3>Comic shop locator</h3>
+            </div>
+            <div class="icon">
+                <div class="img">
+                    <img style="height: 40px" src="../assets/buy-dc-power-visa.svg" alt>
+                </div>
+                <h3>DC power visa</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.menu_dc {
+    background-color: #0282F9;
+    color: white;
+    display: flex;
+    align-items: center;
+    height: 150px;
+    .container {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        .icon {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            img {
+                width: 100%;
+                height: 60px;
+            }
+            h3 {
+                text-transform: uppercase;
+                font-weight: 400;
+                font-size: 15px;
+                margin-left: 10px;
+            }
+        }
+    }
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -5,46 +5,16 @@
                 <h2>&rarr; Content goes here &larr;</h2>
             </div>
         </div>
-        <div class="menu_dc">
-            <div class="container">
-                <div class="icon">
-                    <div class="img">
-                        <img src="../assets/buy-comics-digital-comics.png" alt>
-                    </div>
-                    <h3>Digital comics</h3>
-                </div>
-                <div class="icon">
-                    <div class="img">
-                        <img src="../assets/buy-comics-merchandise.png" alt>
-                    </div>
-                    <h3>DC merchandise</h3>
-                </div>
-                <div class="icon">
-                    <div class="img">
-                        <img src="../assets/buy-comics-subscriptions.png" alt>
-                    </div>
-                    <h3>Subscription</h3>
-                </div>
-                <div class="icon">
-                    <div class="img">
-                        <img src="../assets/buy-comics-shop-locator.png" alt>
-                    </div>
-                    <h3>Comic shop locator</h3>
-                </div>
-                <div class="icon">
-                    <div class="img">
-                        <img style="height: 40px" src="../assets/buy-dc-power-visa.svg" alt>
-                    </div>
-                    <h3>DC power visa</h3>
-                </div>
-            </div>
-        </div>
+        <SiteIcons/>
     </main>
 </template>
 
 <script>
+import SiteIcons from './SiteIcons.vue'
 export default {
-
+    components: {
+        SiteIcons
+    }
 }
 </script>
 
@@ -58,33 +28,6 @@ export default {
         h2 {
             color: white;
             line-height: 150px;
-        }
-    }
-    .menu_dc {
-        background-color: #0282F9;
-        color: white;
-        display: flex;
-        align-items: center;
-        height: 150px;
-        .container {
-            display: flex;
-            justify-content: space-evenly;
-            align-items: center;
-            .icon {
-                display: flex;
-                justify-content: flex-start;
-                align-items: center;
-                img {
-                    width: 100%;
-                    height: 60px;
-                }
-                h3 {
-                    text-transform: uppercase;
-                    font-weight: 400;
-                    font-size: 15px;
-                    margin-left: 10px;
-                }
-            }
         }
     }
 }
