5c6e1a3a3f261977d6b1da999d6eb7a01d57f2bb
xXSamXx00 <samudichio@gmail.com>
2021-11-12 12:01:06 +0100

    Immagini da fixare

---
+++ src/components/Serie.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="card">
+            <img :src="img" alt>
+            <h3>{{ title }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        img: String,
+        title: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+    color: white;
+    text-transform: uppercase;
+    font-size: 12px;
+    img {
+        height: 200px;
+    }
+}
+</style>
